53f36c5982286b8df0e02979d87fe856d8d9cc5f
PriLe <ekamoto.priscila@gmail.com>
2026-01-10 18:11:17 -0400

    Create endpoint GetPessoasNome

---
+++ api/api/Application/Pessoas/Queries/GetAllPessoasNomeQuery.cs
@@ -0,0 +1,21 @@
+﻿using api.Shared.Dtos;
+using api.Shared.Mediator.Interfaces;
+
+namespace api.Application.Pessoas.Queries
+{
+    public class GetAllPessoasNomeQuery : IQuery<List<PessoaNomeDto>>
+    {
+        public GetAllPessoasNomeQuery()
+        {
+        }
+
+        public GetAllPessoasNomeQuery(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+
+        public int Id { get; set; }
+        public string Nome { get; set; } = null!;
+    }
+}
---
+++ api/api/Shared/Dtos/PessoaNomeDto.cs
@@ -0,0 +1,8 @@
+﻿namespace api.Shared.Dtos
+{
+    public class PessoaNomeDto
+    {
+        public  int Id { get; set; }
+        public string Nome { get; set; } = null!;
+    }
+}
