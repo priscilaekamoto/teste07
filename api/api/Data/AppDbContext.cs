49d9c0a1e087ae31064652e665fff9d195723f8c
PriLe <ekamoto.priscila@gmail.com>
2026-01-12 22:09:13 -0400

    Adjust banco entity Transacao

--- api/api/Data/AppDbContext.cs
+++ api/api/Data/AppDbContext.cs
@@ -12,24 +12,8 @@ namespace api.Data
         public DbSet<Transacao> Transacoes { get; set; } = null!;
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Pessoa>(entity =>
-            {
-                entity.HasKey(p => p.Id);
-                entity.Property(p => p.Nome).IsRequired();
-                entity.Property(p => p.Idade).IsRequired();
-            });
-
             modelBuilder.Entity<Transacao>(entity =>
             {
-                entity.HasKey(t => t.Id);
-
-                entity.Property(t => t.Descricao)
-                      .IsRequired()
-                      .HasMaxLength(500);
-
-                entity.Property(t => t.Valor)
-                      .HasColumnType("decimal(6,2)");
-
                 // FK Categoria
                 entity.HasOne(t => t.Categoria)
                       .WithMany(c => c.Transacoes)
---
+++ api/api/Migrations/20260113020512_AlterTableTransacao.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterTableTransacao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Valor",
+                table: "Transacoes",
+                type: "decimal(65,30)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(6,2)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Transacoes",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(500)",
+                oldMaxLength: 500)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Valor",
+                table: "Transacoes",
+                type: "decimal(6,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(65,30)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Transacoes",
+                type: "varchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
